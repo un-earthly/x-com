44dd20def23a40c09912dbb3d2550962713ee88c
un-earthly <vijayalamin@gmail.com>
2023-12-26 07:43:43 +0600

    header animation

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
-
+'use client'
+import React, { useState } from 'react'
+import {motion} from 'framer-motion'
 export default function Intro() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [isInView, setIsInView] = useState(false);
+    
+const hiddenMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)`;
+const visibleMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)`;
+
     return (
         <section className="bg-white dark:bg-gray-900">
             <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
@@ -11,7 +18,20 @@ export default function Intro() {
                     <button className="bg-[#8000ff] text-white text-base px-3 py-2 my-5 rounded-full ">Get Started</button>
                 </div>
                 <div className="grid grid-cols-2 gap-4 mt-8">
-                    <img className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
+                    <motion.div
+                        initial={false}
+                        animate={
+                            isLoaded && isInView
+                                ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
+                                : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+                        }
+                        transition={{ duration: 1, delay: 1 }}
+                        viewport={{ once: true }}
+                        onViewportEnter={() => setIsInView(true)}
+                    >
+                        <img onLoad={() => setIsLoaded(true)} className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
+                    </motion.div>
+
                     <img className="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2" />
                 </div>
             </div>
